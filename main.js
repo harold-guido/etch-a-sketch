a906e5a93a0444aeef79e906c12f0b01d445158d
Harold Parra <gc.harold.parra@gmail.com>
2020-04-20 23:51:15 +0100

    butons now trigger the right function and resize button prompts user for desired size of grid

--- main.js
+++ main.js
@@ -1,25 +1,19 @@
-// function makeGridArray() {
-
-// }
-
-// function percentageOfSpace() {
-
-// }
-
-// const btn = document.querySelector
-
 function toggleActive(element) {
   element.target.classList.toggle("active-grid-div");
 }
 
+// CREATING GRID
+
+// Default Grid
+
 const gridContainer = document.querySelector("#grid-container");
-const gridDivs = Array(16)
+const gridDivs = Array(16 * 16)
   .join(0)
   .split(0)
   .map(() => document.createElement("div"));
 
-// gridContainer.style["grid-template-rows"] = ``;
-// gridContainer.style["grid-template-rows"] = ``;
+gridContainer.style["grid-template-columns"] = "repeat(16, 1fr)";
+gridContainer.style["grid-template-rows"] = "repeat(16, 1fr)";
 
 for (var i = 0; i < gridDivs.length; i++) {
   gridDivs[i].addEventListener("click", toggleActive);
@@ -29,4 +23,42 @@ for (var i = 0; i < gridDivs.length; i++) {
   gridContainer.appendChild(gridDivs[i]);
 }
 
-// .setAttribute("class", "grid-div")
+//FUNCTIONALITY OF GRID
+
+// Resize Button
+
+const resizeBtn = document.querySelector("#resize");
+resizeBtn.addEventListener("click", () => {
+  var validInput = false;
+
+  while (validInput == false) {
+    var resizeInt = Number(
+      prompt(
+        "Please enter an integer value between 10 and 100 \nfor the number of pixels you want per side of canvas"
+      )
+    );
+    if (typeof resizeInt != "number") {
+      validInput = false;
+    } else if (resizeInt < 10 || resizeInt > 100) {
+      validInput = false;
+    } else {
+      validInput = true;
+      resize(Math.floor(resizeInt));
+    }
+  }
+});
+
+// Reset Button
+
+const resetBtn = document.querySelector("#reset");
+resetBtn.addEventListener("click", reset);
+
+// Resize Function
+
+function resize(num) {
+  alert("valid input " + num);
+}
+
+// Reset Function
+
+function reset() {}
