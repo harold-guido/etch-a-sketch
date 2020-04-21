3d5bfa528f82acd4249d5427eeecf71453ce089c
Harold Parra <gc.harold.parra@gmail.com>
2020-04-21 06:38:20 +0100

    implemented other drawing methods: random colors and shades of black

--- main.js
+++ main.js
@@ -19,16 +19,68 @@ function resize(num) {
 
   for (var i = 0; i < gridDivs.length; i++) {
     gridDivs[i].addEventListener("mousedown", function (e) {
-      drawing = true;
-      e.target.classList.add("active-grid-div");
+      if (normal == true) {
+        drawing = true;
+        e.target.style.backgroundColor = `rgb(0, 0, 0)`;
+      } else if (random == true) {
+        drawing = true;
+        e.target.style.backgroundColor = `rgb(${
+          Math.floor(Math.random() * 255) + 1
+        }, ${Math.floor(Math.random() * 255) + 1}, ${
+          Math.floor(Math.random() * 255) + 1
+        })`;
+      } else if (shades == true) {
+        drawing = true;
+        var rgb = "";
+        if (e.target.style.backgroundColor == false) {
+          var rgb = "rgb(255, 255, 255)";
+        } else {
+          rgb = e.target.style.backgroundColor;
+        }
+        var currentRbg =
+          rgb.length == 18
+            ? Number(rgb.substring(4, 7))
+            : rgb.length == 15
+            ? Number(rgb.substring(4, 6))
+            : 0;
+        e.target.style.backgroundColor = `rgb(${
+          currentRbg > 1 ? currentRbg - 25 : currentRbg
+        },${currentRbg > 1 ? currentRbg - 25 : currentRbg},${
+          currentRbg > 1 ? currentRbg - 25 : currentRbg
+        })`;
+        console.log(e.target.style.backgroundColor);
+      }
     });
+
     document.addEventListener("mouseup", function (e) {
       drawing = false;
-      e.target.classList.add("active-grid-div");
     });
     gridDivs[i].addEventListener("mouseover", function (e) {
-      if (drawing == true) {
-        e.target.classList.add("active-grid-div");
+      if (drawing == true && normal == true) {
+        e.target.style.backgroundColor = `rgb(00, 00, 00)`;
+      } else if (drawing == true && random == true) {
+        e.target.style.backgroundColor = `rgb(${
+          Math.floor(Math.random() * 255) + 1
+        }, ${Math.floor(Math.random() * 255) + 1}, ${
+          Math.floor(Math.random() * 255) + 1
+        })`;
+      } else if (drawing == true && shades == true) {
+        if (e.target.style.backgroundColor == false) {
+          var rgb = "rgb(255, 255, 255)";
+        } else {
+          rgb = e.target.style.backgroundColor;
+        }
+        var currentRbg =
+          rgb.length == 18
+            ? Number(rgb.substring(4, 7))
+            : rgb.length == 15
+            ? Number(rgb.substring(4, 6))
+            : 0;
+        e.target.style.backgroundColor = `rgb(${
+          currentRbg > 1 ? currentRbg - 25 : currentRbg
+        },${currentRbg > 1 ? currentRbg - 25 : currentRbg},${
+          currentRbg > 1 ? currentRbg - 25 : currentRbg
+        })`;
       }
     });
     gridDivs[i].setAttribute("class", "grid-div");
@@ -38,14 +90,20 @@ function resize(num) {
 function reset() {
   var gridDivs = document.querySelectorAll(".grid-div");
   for (i = 0; i < gridDivs.length; i++) {
-    gridDivs[i].classList.remove("active-grid-div");
+    gridDivs[i].style.backgroundColor = "";
   }
 }
 
-var drawing = false;
+var drawing = false,
+  normal = true,
+  random = false,
+  shades = false;
 
-const resizeBtn = document.querySelector("#resize"),
-  resetBtn = document.querySelector("#reset");
+const resizeBtn = document.querySelector("#resize");
+const normalBtn = document.querySelector("#normal");
+const randomBtn = document.querySelector("#random");
+const shadesBtn = document.querySelector("#shades");
+const resetBtn = document.querySelector("#reset");
 
 resizeBtn.addEventListener("click", () => {
   var validInput = false;
@@ -65,6 +123,21 @@ resizeBtn.addEventListener("click", () => {
       resize(Math.floor(resizeInt));
     }
   }
+});
+normalBtn.addEventListener("click", () => {
+  normal = true;
+  random = false;
+  shades = false;
+});
+randomBtn.addEventListener("click", () => {
+  random = true;
+  normal = false;
+  shades = false;
+});
+shadesBtn.addEventListener("click", () => {
+  shades = true;
+  normal = false;
+  random = false;
 });
 resetBtn.addEventListener("click", reset);
 
